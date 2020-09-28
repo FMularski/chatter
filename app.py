35437cdac109c867e59725ca5e7e74b2f66d3374
Filip Mularski <mularskif@gmail.com>
2020-09-29 00:04:37 +0200

    create_account.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,19 @@ def login():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'GET':
+        return render_template('create_account.html')
+    else:
+        login_ = request.form['login']
+        email = request.form['email']
+        password = request.form['password']
+        confirm = request.form['confirm']
+
+        return f'{login_}\n{email}\n{password}\n{confirm}'
 
 
 if __name__ == '__main__':
