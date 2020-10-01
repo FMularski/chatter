518cd0f2996fb7057f564a29b4a63c0713d3dcc0
Filip Mularski <mularskif@gmail.com>
2020-10-01 17:54:05 +0200

    friends.html

--- app.py
+++ app.py
@@ -48,5 +48,13 @@ def find_chat():
         pass
 
 
+@app.route('/friends', methods=['GET', 'POST'])
+def friends():
+    if request.method == 'GET':
+        return render_template('friends.html')
+    else:
+        pass
+
+
 if __name__ == '__main__':
     app.run(debug=True)
